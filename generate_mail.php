7f17cae20282ab024bebc74605634e0d9d40211b
Ilia Goloveshko <i.goloveshko@it.ecommpay.com>
2023-12-02 23:09:54 +0700

    first commit

---
+++ generate_mail.php
@@ -0,0 +1,21 @@
+<?php
+
+$sourceFile = dirname(__FILE__) . '/source.txt';
+$sourceFileContent = file_get_contents($sourceFile);
+$sourceFileContent = str_replace("\n", ' ', $sourceFileContent);
+$sentences = explode('.', $sourceFileContent);
+foreach ($sentences as &$sentence) {
+    $sentence = trim($sentence) . '.';
+}
+
+$subject = $sentences[rand(0, count($sentences) - 1)];
+$subject = substr($subject, 0, 75);
+$subjectWords = explode(' ', $subject);
+unset($subjectWords[count($subjectWords) - 1]);
+$subject = implode(' ', $subjectWords);
+
+$bodyRand = rand(0, count($sentences) - 1);
+$bodySentences = array_splice($sentences, $bodyRand, 3);
+$body = implode(' ', $bodySentences);
+
+echo json_encode(['subject' => $subject, 'body' => $body]);
