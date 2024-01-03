d5143ef85eb465ac476d1dbaba042397164c1e94
Ilia Goloveshko <i.goloveshko@it.ecommpay.com>
2024-01-03 13:59:18 +0700

    added reload button

--- html/index.php
+++ html/index.php
@@ -17,18 +17,20 @@ $mail = $mailGenerator->generate();
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="/css/bootstrap.min.css" rel="stylesheet">
-    <title>Document</title>
+    <title>Mali generator</title>
 </head>
 <body>
 <div class="container mt-3">
     <div class="mb-3">
         <label for="subject" class="form-label">Subject</label>
-        <input class="form-control" type="text" readonly id="subject" value="<?= $mail['subject'] ?>">
+        <input class="form-control form-control-lg" type="text" readonly id="subject" value="<?= $mail['subject'] ?>">
     </div>
     <div class="mb-3">
         <label for="body" class="form-label">Body</label>
-        <textarea class="form-control" readonly id="body"><?= $mail['body'] ?></textarea>
+        <textarea class="form-control form-control-lg" readonly id="body"><?= $mail['body'] ?></textarea>
     </div>
+    <div class="mb-3">
+    <a class="btn btn-outline-primary btn-lg" onclick="location.reload(); return false;">Reload</a>
 </div>
 </body>
 </html>
